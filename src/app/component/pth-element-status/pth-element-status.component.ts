f6dbcfe27c9e60c04b48559e258fc5ed19075189
seeker4sun <seeker4sun@gmail.com>
2023-02-23 14:10:50 +0900

    pth element 폴더구조 정리

--- src/app/component/pth-element-status/pth-element-status.component.ts
+++ src/app/component/pth-element-status/pth-element-status.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-pth-element-status',
   templateUrl: './pth-element-status.component.html',
-  styleUrls: ['./pth-element-status.component.scss']
+  styleUrls:  ['./pth-element-status.component.scss']
 })
 export class PthElementStatusComponent {
   user = { id: '' };

133644b0efd9e96e08b0b6010b4743a97bce0204
seeker4sun <seeker4sun@gmail.com>
2023-02-23 13:36:56 +0900

    built-in validator 디렉토리바꿈

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,8 @@ import { PthOrderByPipe } from './pipes/pth-orderBy/pth-order-by.pipe';
 import { PthFilterComponent  } from './component/pth-filter/pth-filter.component';
 import { PthLimitToComponent } from './component/pth-limit-to/pth-limit-to.component';
 import { PthOrderByComponent } from './component/pth-orderBy/pth-order-by/pth-order-by.component';
-import { PthBuiltInValidatorComponent } from './component/pth-built-in-validator/pth-built-in-validator/pth-built-in-validator.component';
-import { PthElementStatusComponent } from './component/pth-element-status/pth-element-status/pth-element-status.component';
+import { PthBuiltInValidatorComponent } from './component/pth-built-in-validator/pth-built-in-validator.component';
+import { PthElementStatusComponent    } from './component/pth-element-status/pth-element-status.component';
 
 @NgModule({
   declarations: [

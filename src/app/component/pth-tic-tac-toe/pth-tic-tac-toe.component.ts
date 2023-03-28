dc4f5cb36cff4defa0358f7a3b6f792829c1d27a
seeker4sun <seeker4sun@gmail.com>
2023-03-28 18:01:18 +0900

    틱텍토 완성

--- src/app/component/pth-tic-tac-toe/pth-tic-tac-toe.component.ts
+++ src/app/component/pth-tic-tac-toe/pth-tic-tac-toe.component.ts
@@ -6,9 +6,10 @@ import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildre
   styleUrls: ['./pth-tic-tac-toe.component.scss']
 })
 export class PthTicTacToeComponent implements AfterViewInit {
-    X_CLASS = 'X';
-    O_CLASS = 'O';
-    WINNING_COMBINATIONS = [
+  X_CLASS = 'X';
+  O_CLASS = 'O';
+  OTurn   = false;
+  WINNING_COMBINATIONS = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -18,50 +19,45 @@ export class PthTicTacToeComponent implements AfterViewInit {
     [0, 4, 8],
     [2, 4, 6]
   ];
-  OTurn = false;
-  cellElements! : NodeListOf<Element>;
-
-  @ViewChild('board'                    ) board!                    : ElementRef;
-  @ViewChild('winningMessage'           ) winningMessageElement!    : ElementRef;
-  @ViewChild('winningMessageTextElement') winningMessageTextElement!: ElementRef;
   
-  constructor(private renderer2: Renderer2){
-    
-  }
+  //cellElements! : NodeListOf<Element>;
+  @ViewChild('board'                    ) board!                    :ElementRef;
+  @ViewChildren('cellsCollection'       ) cellElements!             :QueryList<ElementRef>;
+  @ViewChild('winningMessage'           ) winningMessageElement!    :ElementRef;
+  @ViewChild('winningMessageTextElement') winningMessageTextElement!:ElementRef;
+  constructor(private renderer2: Renderer2){ }
+
   ngAfterViewInit(): void {
-    this.cellElements = this.board.nativeElement.querySelectorAll('[data-cell]');
-    //console.log( this.board.nativeElement.innerText);
-    //console.log( this.cellElements.length );
-    //.nativeElement.innerText);
-    //console.log( this.winningMessageTextElement.nativeElement.innerText );
     this.StartGame();
   }
 
   StartGame(){
     this.OTurn = false;
-
+    const callHandleClick  = (ev:MouseEvent) => { this.handleClick(ev); };
     this.cellElements.forEach( cell => {
-      //console.log(cell);
-      //this.renderer2.removeClass(cell, this.X_CLASS);
-      //this.renderer2.removeClass(cell, this.O_CLASS);
-      //this.renderer2.listen( cell.nativeElement, 'click', this.handleClick, {once:true});
-      //cell.addEventListener('click', this.handleClick, {once: true});
-      cell.classList.remove(this.X_CLASS);
-      cell.classList.remove(this.O_CLASS);
-      cell.addEventListener('click', event => { this.handleClick(event); }, {once: true}); 
+      const cellEl = cell.nativeElement;
+      this.renderer2.removeClass(cellEl, this.X_CLASS);
+      this.renderer2.removeClass(cellEl, this.O_CLASS);
+      this.renderer2.listen     (cellEl, 'click', callHandleClick );
+      
+      // cell.nativeElement.classList.remove(this.X_CLASS);
+      // cell.nativeElement.classList.remove(this.O_CLASS);
+      // cell.nativeElement.addEventListener('click', callHandleClick, {once: true}); 
     });
     this.setBoardHoverClass();
-    //this.renderer2.removeClass(this.winningMessageTextElement.nativeElement, 'show');
-    this.winningMessageElement.nativeElement.classList.remove('show');
+    this.renderer2.removeClass(this.winningMessageElement.nativeElement, 'show');
+    //this.winningMessageElement.nativeElement.classList.remove('show');
   }
 
-  handleClick( e:any ) {
-    //console.log("handler clicked")
-    const cell = e.target as HTMLDivElement;
+  handleClick(ev :MouseEvent){
+    ev.preventDefault();
+    ev.stopPropagation();
+    const cell         = ev.target as HTMLDivElement;
     const currentClass = this.OTurn ? this.O_CLASS : this.X_CLASS;
     // placeMark
-    this.palceMark(cell, currentClass);
-    
+    const alreadyMarked :boolean = this.palceMark(cell, currentClass);
+    if   (alreadyMarked) return;
+
     if (this.checkWin(currentClass)){
       // Check for Win
       this.endGame(false);
@@ -75,22 +71,29 @@ export class PthTicTacToeComponent implements AfterViewInit {
     }
   }
 
-  endGame(draw: boolean){
-    this.winningMessageTextElement.nativeElement.innerText = (draw) ? 'Draw!' :
-      `${this.OTurn ? "O's" : "X's"} Wins!` ;
-    //this.renderer2.addClass( this.winningMessageTextElement.nativeElement, 'show');
-    this.winningMessageElement.nativeElement.classList.add('show')  
+  endGame(draw :boolean){
+    const OX_Wins :string = `${this.OTurn ? "O's" : "X's"} Wins!`
+    this.winningMessageTextElement.nativeElement.innerText = (draw) ? 'Draw!' : OX_Wins;
+    this.renderer2.addClass(this.winningMessageElement.nativeElement, 'show');
+    //this.winningMessageElement.nativeElement.classList.add('show')  
   }
   
   isDraw() :boolean {
-    return Array.from(this.cellElements).every( cell => {
-      return cell.classList.contains(this.X_CLASS) ||
-             cell.classList.contains(this.O_CLASS);
+    const  arrCellElements = this.cellElements.toArray();
+    return arrCellElements.every( cell => {
+      // return cell.classList.contains(this.X_CLASS) ||
+      //        cell.classList.contains(this.O_CLASS);
+      const  clsList = cell.nativeElement.classList;
+      return clsList.contains(this.X_CLASS) ||
+             clsList.contains(this.O_CLASS);
     });
   }
   
-  palceMark(cell:HTMLDivElement, currentClass:string){
-    cell.classList.add(currentClass);
+  palceMark(cell :HTMLDivElement, currentClass :string):boolean{
+    const  clsList                = cell.classList;
+    const  alreadyMarked :boolean = clsList.contains(this.O_CLASS) || clsList.contains(this.X_CLASS);
+    if   (!alreadyMarked){ clsList.add(currentClass); }
+    return alreadyMarked;
   }
   
   swapTurns(){
@@ -100,17 +103,22 @@ export class PthTicTacToeComponent implements AfterViewInit {
   setBoardHoverClass(){
     const XO_cls  = (this.OTurn) ? this.O_CLASS : this.X_CLASS;
     const boardEl = this.board.nativeElement;
-    const clsList = boardEl.classList;
-    clsList.remove(this.X_CLASS);
-    clsList.remove(this.O_CLASS);
-    clsList.add(XO_cls);
+    // const clsList = boardEl.classList;
+    // clsList.remove(this.X_CLASS);
+    // clsList.remove(this.O_CLASS);
+    // clsList.add(XO_cls);
+    this.renderer2.removeClass(boardEl, this.X_CLASS);
+    this.renderer2.removeClass(boardEl, this.O_CLASS);
+    this.renderer2.addClass   (boardEl, XO_cls);
   }
   
-  checkWin(currentClass :string) {
-    const arrCellElements = Array.from(this.cellElements);
+  checkWin(currentClass :string){
+    //const arrCellElements = Array.from(this.cellElements);
+    const arrCellElements = this.cellElements.toArray();
     return this.WINNING_COMBINATIONS.some(combination => {
       return combination.every(index => {
-        return arrCellElements[index].classList.contains(currentClass);
+        //return arrCellElements[index].classList.contains(currentClass);
+        return arrCellElements[index].nativeElement.classList.contains(currentClass);
       });
     });
   }
